ff3536494b82c2daf8a8cc0f23081e467791dcbb
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-11 10:26:58 +0200

    Lean setup process

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-aws s3 cp s3://neurips-aido3-awsdeepracer/build.tar.gz .
-tar xvfz build.tar.gz
-curl https://raw.githubusercontent.com/aws-samples/aws-deepracer-workshops/master/Advanced%20workshops/AI%20Driving%20Olympics%202019/challenge_train_w_PPO/sim_app_bundler.py -o sim_app_bundler.py
 aws s3 cp s3://deepracer-managed-resources-us-east-1/deepracer-simapp.tar.gz .
-mkdir deepracer-simapp
-tar xvfz deepracer-simapp.tar.gz -C deepracer-simapp
-rm -rf build/simapp/bundle/*
-tar xvf deepracer-simapp/bundle.tar -C build/simapp/bundle
-cd build/simapp/bundle/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes
-mv meshes meshes.org
-ln -s meshes.org meshes
+curl https://raw.githubusercontent.com/aws-samples/aws-deepracer-workshops/master/Advanced%20workshops/AI%20Driving%20Olympics%202019/challenge_train_w_PPO/sim_app_bundler.py -o sim_app_bundler.py
+python3 sim_app_bundler.py --untar deepracer-simapp.tar.gz
+DIR="build/simapp/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment"
+mv $DIR/meshes $DIR/meshes.org
+ln -s ./meshes.org $DIR/meshes
