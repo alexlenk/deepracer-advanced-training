c818118aadf635b0eb3e435f133561e557a25abd
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-17 13:56:06 +0200

    Added copy command

--- randomize_world.sh
+++ randomize_world.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export
+cp -rf /home/robomaker/workspace/applications/simulation-application/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes.org /home/robomaker/meshes
 cp /home/robomaker/workspace/applications/simulation-application/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes.org/China_track/textures/China_track_landmark_U_07.png /home/robomaker/meshes/Spain_track/textures/Spain_Track_field_grass_T_01.png
