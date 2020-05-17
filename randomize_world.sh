ecd444fd0cee1f856a842849f18e323da9469920
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-17 18:06:41 +0200

    Added more time

--- randomize_world.sh
+++ randomize_world.sh
@@ -53,7 +53,7 @@ echo Road: Replacing ${ROADS[$WORLD]} with $TEX_ROAD
 cp /home/robomaker/meshes/$TEX_ROAD /home/robomaker/meshes/${ROADS[$WORLD]}
 cp /home/robomaker/meshes/$TEX_GRASS /home/robomaker/meshes/${GRASS[$WORLD]}
 
-TIMER=600
+TIMER=1200
 echo Setting restart timer to $TIMER seconds ...
 /bin/bash -c "sleep $TIMER;reboot" &
 
