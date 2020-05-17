c9119a14dbb850310256cc1e43ccf39856b2adf7
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-17 14:30:32 +0200

    Added debug code

--- randomize_world.sh
+++ randomize_world.sh
@@ -6,4 +6,5 @@ cp -rf /home/robomaker/workspace/applications/simulation-application/bundle/opt/
 ls -al /home/robomaker/workspace/applications/simulation-application/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes/
 echo Modifying Grass ...
 cp /home/robomaker/workspace/applications/simulation-application/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes.org/China_track/textures/China_track_landmark_U_07.png /home/robomaker/meshes/Spain_track/textures/Spain_Track_field_grass_T_01.png 
+ps aux
 echo Done!
