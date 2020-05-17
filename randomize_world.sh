78d09df6be27c403a903208c596ab3d88c1138f0
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-17 13:51:23 +0200

    Add randomise script to be called from the application

---
+++ randomize_world.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export
+cp /home/robomaker/workspace/applications/simulation-application/bundle/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes.org/China_track/textures/China_track_landmark_U_07.png /home/robomaker/meshes/Spain_track/textures/Spain_Track_field_grass_T_01.png
