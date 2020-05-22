c0ab6e67e84944d5215f149312c2163280d10d4c
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-22 09:14:34 +0200

    Fixed world randomisation

--- randomize_world.py
+++ randomize_world.py
@@ -26,8 +26,8 @@ if not path.isfile('/home/robomaker/randomize_world.sh') and os.environ["JOB_TYP
     print("################## Executing randomize_world.sh ##################")
     print("Downloading script ...")
     WORLDS=["New_York_Track", "China_track", "Virtual_May19_Train_track", "Mexico_track", "Tokyo_Training_track", "Canada_Training", "Bowtie_track"]
-    os.environ["WORLD_NAME"] = random.choice(WORLDS)
-    p = subprocess.Popen("curl https://raw.githubusercontent.com/alexlenk/deepracer-advanced-training/master/randomize_world.sh -o /home/robomaker/randomize_world.sh \n /bin/bash /home/robomaker/randomize_world.sh " + os.environ["WORLD_NAME"], stdout=subprocess.PIPE, shell=True)
+    train_world = random.choice(WORLDS)
+    p = subprocess.Popen("curl https://raw.githubusercontent.com/alexlenk/deepracer-advanced-training/master/randomize_world.sh -o /home/robomaker/randomize_world.sh \n /bin/bash /home/robomaker/randomize_world.sh " + train_world, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
     print(output)
@@ -83,6 +83,7 @@ if not path.isfile('/home/robomaker/randomize_world.sh') and os.environ["JOB_TYP
         p = subprocess.Popen("echo '" + result + "' | aws s3 cp - s3://" + os.environ["SAGEMAKER_SHARED_S3_BUCKET"] + "/" + os.environ["SAGEMAKER_SHARED_S3_PREFIX"] + "/" + os.environ["S3_YAML_NAME"], stdout=subprocess.PIPE, shell=True)
     else:
         print("Staying with Job Type to TRAINING")
+        os.environ["WORLD_NAME"] = train_world
         os.environ["JOB_TYPE"] = "TRAINING"
         p = subprocess.Popen("echo 'EVALUATION' | aws s3 cp - s3://" + os.environ["SAGEMAKER_SHARED_S3_BUCKET"] + "/" + os.environ["SAGEMAKER_SHARED_S3_PREFIX"] + "/jobtype", stdout=subprocess.PIPE, shell=True)
         restart_time = 1800
