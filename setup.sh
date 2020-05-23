90cb895af66422234c9680a9392440cfc115112d
Alex Lenk <alexander.lenk@lenkmail.de>
2020-05-23 13:37:47 +0200

    Attach S3 Full Access Policy to Deepracer Robomaker role

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aws iam attach-role-policy --role-name="AWSDeepRacerRoboMakerAccessRole" --policy-arn="arn:aws:iam::aws:policy/AmazonS3FullAccess"
 aws s3 cp s3://deepracer-managed-resources-us-east-1/deepracer-simapp.tar.gz .
 curl https://raw.githubusercontent.com/aws-samples/aws-deepracer-workshops/master/Advanced%20workshops/AI%20Driving%20Olympics%202019/challenge_train_w_PPO/sim_app_bundler.py -o sim_app_bundler.py
 python3 sim_app_bundler.py --untar deepracer-simapp.tar.gz
